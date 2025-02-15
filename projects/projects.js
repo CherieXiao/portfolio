31e75c13d04b43087bd83ed3c29a4e93c40d754f
CherieXiao <CherieXiao2@hotmail.com>
2025-02-15 02:37:56 -0800

    1

--- projects/projects.js
+++ projects/projects.js
@@ -7,39 +7,41 @@ renderProjects(projects, projectsContainer, 'h2');
 const projectsTitle = document.querySelector('.projects-title');
 projectsTitle.textContent = `${projects.length} Projects`;
 
-let data = [
-    { value: 1, label: 'apples' },
-    { value: 2, label: 'oranges' },
-    { value: 3, label: 'mangos' },
-    { value: 4, label: 'pears' },
-    { value: 5, label: 'limes' },
-    { value: 5, label: 'cherries' },
-  ];
+let data = rolledData.map(([year, count]) => {
+  return { value: count, label: year };
+});
   
-  let sliceGenerator = d3.pie().value((d) => d.value);
+let sliceGenerator = d3.pie().value((d) => d.value);
   
-  let colors = d3.scaleOrdinal(d3.schemeTableau10);
+let colors = d3.scaleOrdinal(d3.schemeTableau10);
   
-  let arcData = sliceGenerator(data);
+let arcData = sliceGenerator(data);
   
-  let arcGenerator = d3.arc()
-    .innerRadius(0)
-    .outerRadius(50);
+let arcGenerator = d3.arc()
+  .innerRadius(0)
+  .outerRadius(50);
   
-  let svg = d3.select('svg');
+let svg = d3.select('svg');
   
-  arcData.forEach((slice, idx) => {
-    svg.append('path')
-      .attr('d', arcGenerator(slice))
-      .attr('fill', colors(idx));
-  });
+arcData.forEach((slice, idx) => {
+  svg.append('path')
+    .attr('d', arcGenerator(slice))
+    .attr('fill', colors(idx));
+});
   
-  let legend = d3.select('.legend');
-  data.forEach((d, idx) => {
-      legend.append('li')
-            .attr('style', `--color:${colors(idx)}`)
-            .attr('class', 'legend')
-            .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`);
-  })
+let legend = d3.select('.legend');
+data.forEach((d, idx) => {
+    legend.append('li')
+          .attr('style', `--color:${colors(idx)}`)
+          .attr('class', 'legend')
+          .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`);
+})
   
+let rolledData = d3.rollups(
+  projects,
+  (v) => v.length,
+  (d) => d.year,
+);
+
+
   
