c38667dde131883d2830af737fac0f98fb1fa59f
CherieXiao <CherieXiao2@hotmail.com>
2025-02-15 05:19:32 -0800

    step2.3

--- projects/projects.js
+++ projects/projects.js
@@ -2,10 +2,18 @@ import { fetchJSON, renderProjects } from '../global.js';
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm";
 
 const projects = await fetchJSON('../lib/projects.json');
+
+
 const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
 const projectsTitle = document.querySelector('.projects-title');
 projectsTitle.textContent = `${projects.length} Projects`;
+
+let rolledData = d3.rollups(
+  projects,
+  (v) => v.length,
+  (d) => d.year,
+);
 
 let data = rolledData.map(([year, count]) => {
   return { value: count, label: year };
@@ -22,6 +30,7 @@ let arcGenerator = d3.arc()
   .outerRadius(50);
   
 let svg = d3.select('svg');
+svg.selectAll('path').remove();
   
 arcData.forEach((slice, idx) => {
   svg.append('path')
@@ -33,15 +42,10 @@ let legend = d3.select('.legend');
 data.forEach((d, idx) => {
     legend.append('li')
           .attr('style', `--color:${colors(idx)}`)
-          .attr('class', 'legend')
           .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`);
 })
   
-let rolledData = d3.rollups(
-  projects,
-  (v) => v.length,
-  (d) => d.year,
-);
+
 
 
   
